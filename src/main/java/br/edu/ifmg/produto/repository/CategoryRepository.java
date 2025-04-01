1f5b38f876b80f62fa0c4be800f0458f4a96d5bd
jo4ovictors <joaovictors1003@gmail.com>
2025-04-01 09:56:52 -0300

    Alterações - Aula 27/03

---
+++ src/main/java/br/edu/ifmg/produto/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifmg.produto.repository;
+
+import br.edu.ifmg.produto.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/br/edu/ifmg/produto/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_category (name) values ('livros');
+insert into tb_category (name) values ('discos');
+insert into tb_category (name) values ('jogos');
