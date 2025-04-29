593c85baa796b1569a4a8ae06afe3ef454706b45
jo4ovictors <joaovictors1003@gmail.com>
2025-04-29 17:47:34 -0300

    Aula 29/04 - junit

--- src/main/java/br/edu/ifmg/produto/services/ProductService.java
+++ src/main/java/br/edu/ifmg/produto/services/ProductService.java
@@ -38,14 +38,12 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Optional<Product> obj = productRepository.findById(id);
         Product product = obj.orElseThrow(() -> new ResourceNotFound("Product not found " + id));
-        return new ProductDTO(product);
-        /*
-                .add(linkTo().withSelfRel())
-                .add(linkTo().withRel("All products"))
-                .add(linkTo().withRel("Update product"))
-                .add(linkTo().withRel("Delete product"))
-                ;
-        */
+        return new ProductDTO(product)
+                .add(linkTo(methodOn(ProductResource.class).findById(product.getId())).withSelfRel())
+                .add(linkTo(methodOn(ProductResource.class).findAll(null)).withRel("All products"))
+                .add(linkTo(methodOn(ProductResource.class).update(product.getId(), null)).withRel("Update product"))
+                .add(linkTo(methodOn(ProductResource.class).delete(product.getId())).withRel("Delete product"));
+
     }
 
     @Transactional
@@ -53,7 +51,11 @@ public class ProductService {
         Product entity = new Product();
         copyDTOToEntity(dto, entity);
         entity = productRepository.save(entity);
-        return new ProductDTO(entity);
+        return new ProductDTO(entity)
+                .add(linkTo(methodOn(ProductResource.class).findById(entity.getId())).withRel("Get a product"))
+                .add(linkTo(methodOn(ProductResource.class).findAll(null)).withRel("All products"))
+                .add(linkTo(methodOn(ProductResource.class).update(entity.getId(), null)).withRel("Update product"))
+                .add(linkTo(methodOn(ProductResource.class).delete(entity.getId())).withRel("Delete product"));
     }
 
     @Transactional
@@ -62,7 +64,10 @@ public class ProductService {
             Product entity = productRepository.getReferenceById(id);
             copyDTOToEntity(dto, entity);
             entity = productRepository.save(entity);
-            return new ProductDTO(entity);
+            return new ProductDTO(entity)
+                    .add(linkTo(methodOn(ProductResource.class).findById(entity.getId())).withRel("Find a product"))
+                    .add(linkTo(methodOn(ProductResource.class).findAll(null)).withRel("All products"))
+                    .add(linkTo(methodOn(ProductResource.class).delete(entity.getId())).withRel("Delete product"));
         }
         catch (EntityNotFoundException e) {
             throw new ResourceNotFound("Product not found " + id);
---
+++ src/test/java/br/edu/ifmg/produto/repository/ProductRepositoryTest.java
@@ -0,0 +1,28 @@
+package br.edu.ifmg.produto.repository;
+
+import br.edu.ifmg.produto.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName(value = "Verificando se o objeto não existe no BD depois de deletado.")
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        productRepository.deleteById(1L);
+        Optional<Product> obj = productRepository.findById(1L);
+
+        Assertions.assertFalse(obj.isPresent());
+
+    }
+}
