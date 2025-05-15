9319ed4e6659ab8459cd9205fc152e66d4f5fda7
jo4ovictors <joaovictors1003@gmail.com>
2025-05-15 17:44:12 -0300

    Aula 15/05 - CRUD Usuário

---
+++ src/main/java/br/edu/ifmg/produto/dtos/UserDTO.java
@@ -0,0 +1,105 @@
+package br.edu.ifmg.produto.dtos;
+
+import br.edu.ifmg.produto.entities.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDTO {
+
+    private Long id;
+    @NotBlank(message = "Campo Obrigatório")
+    private String firstName;
+    private String lastName;
+
+    @Email(message = "Favor informar um email válido")
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UserDTO(UserDTO entity) {
+        this.id = entity.getId();
+        this.firstName = entity.getFirstName();
+        this.lastName = entity.getLastName();
+        this.email = entity.getEmail();
+    }
+
+    public UserDTO(User user) {
+        id = user.getId();
+        firstName = getFirstName();
+        lastName = getLastName();
+        email = user.getEmail();
+
+        user.getRoles().forEach( role -> roles.add(new RoleDTO(role)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleDTO> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+
+}
---
+++ src/main/java/br/edu/ifmg/produto/services/UserService.java
@@ -0,0 +1,104 @@
+package br.edu.ifmg.produto.services;
+
+import br.edu.ifmg.produto.dtos.ProductDTO;
+import br.edu.ifmg.produto.dtos.RoleDTO;
+import br.edu.ifmg.produto.dtos.UserDTO;
+import br.edu.ifmg.produto.dtos.UserInsertDTO;
+import br.edu.ifmg.produto.entities.Product;
+import br.edu.ifmg.produto.entities.Role;
+import br.edu.ifmg.produto.entities.User;
+import br.edu.ifmg.produto.repository.RoleRepository;
+import br.edu.ifmg.produto.repository.UserRepository;
+import br.edu.ifmg.produto.resources.ProductResource;
+import br.edu.ifmg.produto.services.exceptions.ResourceNotFound;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAll(Pageable pageable) {
+        Page<User> list = repository.findAll(pageable);
+        return list.map(u -> new UserDTO(u));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> opt = repository.findById(id);
+        User user = opt.orElseThrow(() -> new ResourceNotFound("User Not Found"));
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public UserDTO insert(UserInsertDTO dto) {
+
+        User entity = new User();
+        copyDtoToEntity(dto, entity);
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+        User novo = repository.save(entity);
+        return new UserDTO(novo);
+
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity) {
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setEmail(dto.getEmail());
+
+        entity.getRoles().clear();
+        for (RoleDTO role : dto.getRoles()) {
+            Role r = roleRepository.getReferenceById(role.getId());
+            entity.getRoles().add(r);
+        }
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserDTO dto) {
+        try {
+            User entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new UserDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFound("User not found " + id);
+        }
+
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if(!repository.existsById(id)) {
+            throw new ResourceNotFound("User not found");
+        }
+
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new ResourceNotFound("Integrity violation");
+        }
+    }
+
+}
